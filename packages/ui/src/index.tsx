bb331724b18b12916d4a413c5288d87769f6c996
Dellil <aksaksdm@naver.com>
2021-11-05 19:56:30 +0900

    Set monorepo environment

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './global.scss';
+
+const Component: React.FC<{}> = ({children}) => {
+  return <div className="asdf">{children}</div>
+};
+
+export default Component;
